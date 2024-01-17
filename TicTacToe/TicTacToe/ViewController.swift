58cf82b49bdd2ff20d5befc5f3d94bd89a3fede6
Mizgin Yıldırak <101987484+MizginYildirak@users.noreply.github.com>
2024-01-18 02:39:44 +0300

    add checkorWin function

--- TicTacToe/TicTacToe/ViewController.swift
+++ TicTacToe/TicTacToe/ViewController.swift
@@ -10,7 +10,6 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var turnLabel: UILabel!
-    
     @IBOutlet weak var a1: UIButton!
     @IBOutlet weak var a2: UIButton!
     @IBOutlet weak var a3: UIButton!
@@ -21,11 +20,54 @@ class ViewController: UIViewController {
     @IBOutlet weak var c2: UIButton!
     @IBOutlet weak var c3: UIButton!
     
+    var buttons: [UIButton] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        buttons = [a1, a2, a3, b1, b2, b3, c1, c2, c3]
+        
+        for button in buttons {
+            button.setTitleColor(UIColor(red: 1/255, green: 100/255, blue: 100/255, alpha: 1), for: .normal)
+            button.titleLabel?.font = .systemFont(ofSize: 50.0, weight: .heavy)
+        }
     }
-
-
+    
+    let firstPlayer = "X"
+    let secondPlayer = "O"
+    var currentPlayer = "X"
+    
+    @IBAction func btnTapped(_ sender: UIButton) {
+        
+        sender.setTitle(currentPlayer, for: .normal)
+        sender.isUserInteractionEnabled = false
+        checkForWin()
+        if (currentPlayer == firstPlayer) {
+            currentPlayer = secondPlayer
+            turnLabel.text = currentPlayer
+        } else {
+            currentPlayer = firstPlayer
+            turnLabel.text = currentPlayer
+            sender.setTitleColor(UIColor(red: 255/255, green: 108/255, blue: 34/255, alpha: 1), for: .normal)
+        }
+       
+    }
+    
+    func checkForWin() {
+            // Check for horizontal victory
+            if a1.title(for: .normal) == currentPlayer && a2.title(for: .normal) == currentPlayer && a3.title(for: .normal) == currentPlayer {
+                // Create a new alert
+             showAlertMessage()
+            }
+        
+            // Check for vertical victory
+        if a1.title(for: .normal) == currentPlayer && b1.title(for: .normal) == currentPlayer && c1.title(for: .normal) == currentPlayer {
+            // Create a new alert
+            showAlertMessage()
+        }
+        
+        func showAlertMessage() {
+            let dialogMessage = UIAlertController(title: "Attention", message: "I am an alert message", preferredStyle: .actionSheet)
+            self.present(dialogMessage, animated: true, completion: nil)
+        }
+        }
 }
-
