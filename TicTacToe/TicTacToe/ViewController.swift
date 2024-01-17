bfa61bc46da57046c2a3002f7f1c653e15cccedc
Mizgin Yıldırak <101987484+MizginYildirak@users.noreply.github.com>
2024-01-17 18:20:25 +0300

    initial commit

---
+++ TicTacToe/TicTacToe/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  TicTacToe
+//
+//  Created by MİZGİN YILDIRAK on 17.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var turnLabel: UILabel!
+    
+    @IBOutlet weak var a1: UIButton!
+    @IBOutlet weak var a2: UIButton!
+    @IBOutlet weak var a3: UIButton!
+    @IBOutlet weak var b1: UIButton!
+    @IBOutlet weak var b2: UIButton!
+    @IBOutlet weak var b3: UIButton!
+    @IBOutlet weak var c1: UIButton!
+    @IBOutlet weak var c2: UIButton!
+    @IBOutlet weak var c3: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
